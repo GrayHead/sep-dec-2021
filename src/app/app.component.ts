7f07e7bbd8906c1e1830076cd4d7dc44235a75ed
GrayHead <ss.zhuravlov@gmail.com>
2022-05-26 20:36:02 +0300

    emit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IUser} from "./models/IUser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sep-dec-2021';
+
+  user: IUser;
+
+  catch(user: IUser) {
+    console.log(user, 'app');
+    this.user = user;
+
+  }
 }
